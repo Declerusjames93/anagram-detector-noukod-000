4d8e55542935657dfaad104a471f5cee4a002fef
James Declerus <james.declerus@esih.edu>
2019-04-26 15:49:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,17 +1,17 @@
 # Your code goes here!
 
+
 class Anagram
+
   attr_accessor :word
-def initialize(word)
-  @word = word
-  @@some_word << word
-end
-@@some_word = []
-def match(some_word)
-  some_word.match{ |X|
-    X.split("")
 
+  def initialize(word)
+    @word = word
+  end
 
-  }
-end
+  def match( some_word)
+      some_word.select do |X|
+      X.split("").sort == @word.split("").sort
+    end
+  end
 end
