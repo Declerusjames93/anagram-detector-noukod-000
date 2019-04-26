97c56339d4c113819054022fcafb42b7c0d6cdd5
James Declerus <james.declerus@esih.edu>
2019-04-26 15:44:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,17 @@
 # Your code goes here!
 
 class Anagram
-  attr_accessor :
+  attr_accessor :word
+def initialize(word)
+  @word = word
+  @@some_word << word
+end
+@@some_word = []
+def match(some_word)
+  some_word.match{ |X|
+    X.split("")
 
+    
+  }
+end 
 end
